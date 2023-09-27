1138f27d19b5a2f3b107d841ccb611abefc2208f
wittalloM <wittallomarcos@gmail.com>
2023-09-27 12:41:39 -0300

    At quiz2 - 50%

--- Scripts/nQuiz.js
+++ Scripts/nQuiz.js
@@ -1,17 +1,18 @@
 
 import questions from './questions.js'
 
-
+const $qst_box = document.getElementById('question-box')
 const $qst_txt = document.getElementById('qst-enun')
-const $asw_box = document.getElementsByClassName('answers-box')
+const $asw_box = document.getElementById('answers-box')
 const $stt_btt = document.getElementById('stt-btt')
 const $nxt_btt = document.getElementById('nxt-btt')
 
-var currentIndex, sort_qst, Placar = 0
+var sort_qst, Placar = 0
+var currentIndex = 0
 
 $stt_btt.addEventListener('click', Start_game)
 sort_qst = questions[currentIndex]
-console.log(questions[sort_qst].question);
+console.log(questions[currentIndex].question);
 
 /* [Função de ordenação de array] 
 function ord_qst(a,b) {
@@ -27,10 +28,11 @@ function ord_qst(a,b) {
 
 
 function Start_game() {
-    $stt_btt.classList.add('hide')
-    $asw_box.classList.remove('hide')
-    $nxt_btt.classList.remove('hide')
-    Show_qst()
+    $stt_btt.classList.add('hide');
+    $nxt_btt.classList.remove('hide');
+    $qst_box.classList.remove('hide')
+    $asw_box.classList.remove('hide');
+    Show_qst();
 }
 
 
@@ -39,20 +41,20 @@ function Show_qst() {
         $asw_box.removeChild($asw_box.firstChild)
     }
 
-    $qst_txt.textContent = questions[sort_qst].question
+    $qst_txt.textContent = questions[currentIndex].question
     questions[currentIndex].answers.forEach(answer => {
         const nw_answer = document.createElement('button')
-        nw_answer.classList.add("asw-btt")
+        nw_answer.classList.add('asw-btt')
         nw_answer.textContent = answer.text
 
-        if (answer.corect) {
+        if (answer.correct) {
             nw_answer.dataset.correct = answer.correct
         }
+        $asw_box.appendChild(nw_answer)
     })
 
 
 
     $nxt_btt.addEventListener('click', Next_qst)
     currentIndex += 1
-    $asw_box.appendChild(nw_answer)
 }
