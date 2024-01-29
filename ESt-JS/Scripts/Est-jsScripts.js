b44443e6ac37ff08ca95ccc72e146ab9500edccd
wittalloM <wittallomarcos@gmail.com>
2024-01-29 14:45:08 -0300

    Att-Home 1.0

--- ESt-JS/Scripts/Est-jsScripts.js
+++ ESt-JS/Scripts/Est-jsScripts.js
@@ -1,18 +1,28 @@
+//Imports de elementos do HTML - Imc - Form
 const Nome_Imc = document.getElementById("name-Imc")
 const Peso_Imc = document.getElementById("peso-Imc")
 const Altura_Imc = document.getElementById("alt-Imc")
 const Art_txt = document.getElementById("Artigo-Texto1")
 const Form_Imc = document.getElementById("Form-Imc")
+const Form_User = document.getElementById("Form-User")
 
-const btt_resetar = document.getElementById("Resetar")
+// User - Form
+const user_Email = document.getElementById("user-email")
+const user_Name = document.getElementById("user-name")
+
+// Botões 
 const Imc_btt = document.getElementById("Btt-Form-Imc")
+const FUser_btt = document.getElementById("Btt-Form-User")
+
+//Div de resposta - Imc
 const Resp_Imc = document.getElementById("Resp-Imc")
 
 
-
+// Listeners
 Imc_btt.addEventListener("click", CkStatus)
-btt_resetar.addEventListener("click", ResetarImputs("Imput-Imc"))
+FUser_btt.addEventListener("click", CkStatus2)
 
+// Função para checkar(Preenchimento) dos inputs
 function CkStatus() {
 
     if (Nome_Imc.value == '' && Peso_Imc.value == '' && Altura_Imc.value == '') {
@@ -23,63 +33,107 @@ function CkStatus() {
     }
     else {
         Calc_Imc()
-        console.log("Função Chamada")
 
     }
 }
 
+// Cálculo do IMC e distr. do resultado 
 function Calc_Imc() {
-    let Imc_valor = 0
-    let vlrAltura = parseFloat(Altura_Imc.value)
-    let vlrPeso = Number(Peso_Imc.value)
     let Nome_usr = Nome_Imc.value
+    let vlrAltura = parseFloat(Altura_Imc.value) * parseFloat(Altura_Imc.value)
+    let vlrPeso = Number(Peso_Imc.value)
+    let Imc_valor = 0
     let message = ` `
+
+    Imc_valor = vlrPeso / vlrAltura
+    Imc_Final = parseInt(Imc_valor)
+    let img_div = document.createElement("img")
+    img_div.setAttribute("src", " ")
+
+
+    if (Imc_Final < 21) {
+        message = `<p>Olá ${Nome_usr}, seu Imc é igual á : ${Imc_Final}, segundo os nossos dados e a tebela fornecida, você se encontra <p class="enfase">abaixo do peso ideal.</p></p>`
+        img_div.setAttribute("src", "../Styles/Imagens/Imc-Negativo (1).png")
+        Imc_btt.textContent = "Resetar Dados"
+        Imc_btt.classList.add("Btt-Form-Imc")
+        Imc_btt.setAttribute("title", "Botão Reset")
+        Imc_btt.addEventListener("click", Resetar)
+    } else if (Imc_Final == 21 || Imc_Final <= 30) {
+        message = `<p>Olá ${Nome_usr}, seu Imc é igual á : ${parseInt(Imc_Final)}, segundo os nossos dados e a tebela fornecida, você se encontra <p class="enfase">dentro do peso ideal.</p></p>`
+        img_div.setAttribute("src", "../Styles/Imagens/Imc-Positivo (1).png")
+        Imc_btt.textContent = "Resetar Dados"
+        Imc_btt.classList.add("Btt-Form-Imc")
+        Imc_btt.setAttribute("title", "Botão Reset")
+        Imc_btt.addEventListener("click", Resetar)
+    }
+    else if (Imc_Final > 30) {
+        message = `<p>Olá ${Nome_usr}, seu Imc é igual á : ${parseInt(Imc_valor)}, segundo os nossos dados e a tebela fornecida, você se encontra <p class="enfase">acima do peso ideal.</p></p>`
+        img_div.setAttribute("src", "../Styles/Imagens/Imc-Exagerado (1).png")
+        Imc_btt.textContent = "Resetar Dados"
+        Imc_btt.classList.add("Btt-Reset")
+        Imc_btt.setAttribute("title", "Botão Reset")
+        Imc_btt.addEventListener("click", Resetar)
+    }
+
+    console.log("O Valor do Imc do usuário é = " + Imc_Final)
     Art_txt.classList.add("hide")
-
-
-    Imc_valor = vlrPeso / vlrAltura * vlrAltura 
-
-    if (Imc_valor <= 19) {
-        message = `<p>Olá ${Nome_usr}, seu Imc é igual á: ${Imc_valor}, segundo os nossos dados e a tebela fornecida, você se encontra <strong class="enfase">abaixo do peso ideal.</strong></p>`
-    } else if (Imc_valor = 20 && Imc_valor < 23) {
-        message = `<p>O seu Imc é igual á: ${Imc_valor}, segundo os nossos dados e a tebela fornecida, você se encontra <strong class="enfase">dentro do peso ideal.</strong></p>`
-    }
-    else if (Imc_valor = 24 && Imc_valor <= 30) {
-        message = `<p>O seu Imc é igual á: ${Imc_valor}, segundo os nossos dados e a tebela fornecida, você se encontra <strong class="enfase">acima do peso ideal.</strong></p>`
-    }
-
-    console.log("O Valor do Imc do usuário é = " + Imc_valor)
     Resp_Imc.innerHTML = `${message}`
+    Resp_Imc.appendChild(img_div)
 }
 
 
-/*
+
 function Resetar() {
-    
+
     var entradas = document.querySelectorAll(".Imc-Item");
     let set = confirm("Deseja apagar os dados no formuláio ?");
     if (set) {
-        alert("Os campos do formuário serão resetados");
-        console.log("Número de elementos no array QuerySelector: " + entradas.length);
-        for (let i = 0; i < entradas.length; i++){
-            entradas[i].value = "";
-        }
+        entradas.forEach((entrada) => entrada.value = "")
+    }
+    Imc_btt.textContent = "Enviar Dados"
+    Imc_btt.addEventListener("click", CkStatus)
+    console.log("Número de elementos no array QuerySelector: " + entradas.length);
+    Resp_Imc.classList.add("hide")
+    Art_txt.classList.remove("hide")
+}
+
+
+
+function CkStatus2() {
+    if (!user_Email.value || !user_Name.value) {
+        window.alert("Por favor, preencha os 2 campos")
+    } else if (user_Email.value == '' && user_Name.value == '') {
+        window.alert("Por favor, preencha os 2 campos indicados corretamente")
+    } else {
+        GUser()
     }
 }
 
-*/
+function GUser() {
+    user_info = Form_User.getElementsByTagName("input")
 
-function ResetarImputs(name){
-    let entradas = document.querySelectorAll("imput[name='"+name+"']");
-    [].map.call(entradas, entrada => entrada.value = "");
+    let User_Name = user_info[0].value
+    let User_Mail = user_info[1].value
+
+    let cad_user = document.createElement("div")
+    let user_messagediv = document.getElementById("user-message")
+
+    user_mess_resp = user_messagediv.value
+    let user_message = user_messagediv.value
+    let message = `<p class= "enfase">Email do Usuário: ${User_Mail}, Nome do usuário: ${User_Name}</p>`
+
+    console.log(user_message);
+    cad_user.innerHTML = `${message}`
+    Form_User.appendChild(cad_user)
+
 }
 
+//Testes 
 /*
-function allReset(){
-    let entradas = document.querySelectorAll(".Imc-Item")
-    entradas.forEach((entrada) => {
-        entrada.value = ""
-    });
-    
-} 
+function ResetarImputs(name) {
+    let entradas = document.querySelectorAll("input [name='" + name + "']");
+    console.log(entradas.length)
+    entradas.entrada.value = ""
+}
 */
+
